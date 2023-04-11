550b85c48a37b7c668a395e6d223bd2e865847f7
Bruno de Sousa <88456899+wupckoding@users.noreply.github.com>
2023-04-11 05:34:24 -0300

    html e css pronto

---
+++ script.js
@@ -0,0 +1,7 @@
+const minutosElement = document.getElementById("minutos");
+const segundosElement = document.getElementById("segundos");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const stopButton = document.getElementById("stop");
+const aumentarButton = document.getElementById("aumentar");
+const diminuirButton = document.getElementById("diminuir");
